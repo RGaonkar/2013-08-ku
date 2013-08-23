8d83e4b013d8e1f0db37c908730b76bdd88dcf8c
Darren Boss <darren.boss@gmail.com>
2013-08-23 10:36:55 -0500

    Made the example more basic

--- argparse/scripts/argparsetest.py
+++ argparse/scripts/argparsetest.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python
-"""
-argparse.py
-
-Created by Darren Boss on 2013-08-23.
-"""
 import argparse
 
-parser = argparse.ArgumentParser(description='this is argparse help test')
-args = parser.parse_args()
+parser = argparse.ArgumentParser()
+parser.parse_args()
